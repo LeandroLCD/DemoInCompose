29c1b6e29ac217241220f41f7284624c704369f0
Leandro Colmenarez <clinicalsystem@Outlook.es>
2023-12-30 19:43:44 -0300

    Se ingreso Buttons.kt Entrys.kt y MyIcons.tk

--- app/src/main/java/com/blipblipcode/demoincompose/MainActivity.kt
+++ app/src/main/java/com/blipblipcode/demoincompose/MainActivity.kt
@@ -4,19 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.blipblipcode.demoincompose.ui.controls.Label
-import com.blipblipcode.demoincompose.ui.controls.MyButtons
-import com.blipblipcode.demoincompose.ui.controls.MyIcons
+import com.blipblipcode.demoincompose.ui.controls.*
 import com.blipblipcode.demoincompose.ui.theme.DemoInComposeTheme
 
 class MainActivity : ComponentActivity() {
